93fa7f18fdf583ed4474c734460fb9362409f68a
S.P. Mohanty <mohanty@aicrowd.comw>
2021-02-17 06:52:19 +0000

    Refactor generator to be dynamic in terms of classes

--- data_generator/mab_e_data_generator.py
+++ data_generator/mab_e_data_generator.py
@@ -5,16 +5,12 @@ from keras.models import Sequential
 import numpy as np
 
 
-classname_to_index_map = {'other': 0,'investigation': 1,
-                   'attack' : 2, 'mount' : 3}
-def classname_to_index(annotations_list):
-  return np.vectorize(classname_to_index_map.get)(annotations_list)
 
 class MABe_Data_Generator(keras.utils.Sequence):
     def __init__(self,  pose_dict, 
                         batch_size = 2,
                         feature_dimensions=(2,2,7),
-                        n_classes=4,
+                        classes=['other', 'investigation', 'attack', 'mount'],
                         past_frames=100,
                         future_frames=100,
                         frame_skip=1,
@@ -22,7 +18,9 @@ class MABe_Data_Generator(keras.utils.Sequence):
 
         self.batch_size = batch_size
         self.feature_dimensions = feature_dimensions
-        self.n_classes=n_classes
+
+        self.classes=classes
+        self.n_classes=len(classes)
 
         self.past_frames = past_frames
         self.future_frames = future_frames
@@ -44,6 +42,9 @@ class MABe_Data_Generator(keras.utils.Sequence):
         # Load raw pose dictionary
         self.load_pose_dictionary(pose_dict)
         
+        # Setup Utilities
+        self.setup_utils()
+
         # Preprocess annotations
         self.preprocess_annotations()
 
@@ -56,37 +57,56 @@ class MABe_Data_Generator(keras.utils.Sequence):
         self.pose_dict = pose_dict
         self.video_keys = list(pose_dict.keys())
 
-    
-    def preprocess_annotations(self):
-        # Define arrays to map video keys to frames 
-        self.video_indexes = []
-        self.frame_indexes = []
+    def setup_utils(self):
+        # Set up padding utilities
+        self.setup_padding_utils()
 
+        # Setup class conversion utils
+        self.setup_class_conversion_utils()
+
+    def setup_padding_utils(self):
         # Prepare to pad frames
         self.left_pad = self.past_frames * self.frame_skip
         self.right_pad = self.future_frames * self.frame_skip
         self.pad_width = (self.left_pad, self.right_pad), (0, 0), (0, 0), (0, 0)
-        
+
+    def setup_class_conversion_utils(self):
+        # Prepare a classname to idx map
+        self.classname_to_index_map = {}
+        for _idx, class_name in enumerate(self.classes):
+            self.classname_to_index_map[class_name] = _idx
+    
+    def classname_to_index(self, annotations_list):
+        """
+        Converts a list of string classnames into numeric indices
+        """
+        return np.vectorize(self.classname_to_index_map.get)(annotations_list)
+
+    def preprocess_annotations(self):
+        # Define arrays to map video keys to frames 
+        self.video_indices = []
+        self.frame_indices = []
+
         self.action_annotations = []
 
-        for vc, key in enumerate(self.video_keys):
-            annotations = self.pose_dict[key]['annotations']
+        for video_index, video_key in enumerate(self.video_keys):
+            annotations = self.pose_dict[video_key]['annotations']
             self.action_annotations.extend(annotations) # add annotations to action_annotations        
 
             number_of_frames = len(annotations)
 
-            self.video_indexes.extend([vc] * number_of_frames) # Keep a record of video_indexes
-            self.frame_indexes.extend(range(number_of_frames)) # Keep a record of frame indexes
-            self.X[key] = np.pad(self.pose_dict[key]['keypoints'], self.pad_width) # Add padded keypoints for each video key
+            self.video_indices.extend([video_index] * number_of_frames) # Keep a record of video_indices
+            self.frame_indices.extend(range(number_of_frames)) # Keep a record of frame indices
+            self.X[video_key] = np.pad(self.pose_dict[video_key]['keypoints'], self.pad_width) # Add padded keypoints for each video key
 
-        self.y = classname_to_index(self.action_annotations) # convert text labels to indices
-        self.X_dtype = self.X[key].dtype # Store D_types of X 
+        self.y = self.classname_to_index(self.action_annotations) # convert text labels to indices
+        self.X_dtype = self.X[video_key].dtype # Store D_types of X 
         
         #generate a global index list for all data points
-        self.indexes = np.arange(len(self.frame_indexes)) 
+        self.indices = np.arange(len(self.frame_indices)) 
 
     def __len__(self):
-        return len(self.indexes) // self.batch_size
+        return len(self.indices) // self.batch_size
 
     def get_X(self, data_index):
         """
@@ -95,10 +115,10 @@ class MABe_Data_Generator(keras.utils.Sequence):
         """
         # Obtain video key for this datapoint
         video_key = self.video_keys[
-            self.video_indexes[data_index]
+            self.video_indices[data_index]
         ]
         # Identify the (local) frame_index
-        frame_index = self.frame_indexes[data_index]
+        frame_index = self.frame_indices[data_index]
         # Slice from beginning of past frames to end of future frames 
         slice_start_index = frame_index - self.left_pad
         slice_end_index = frame_index + 1 + self.right_pad 
@@ -111,7 +131,7 @@ class MABe_Data_Generator(keras.utils.Sequence):
         
     def __getitem__(self, index):
         batch_size = self.batch_size
-        batch_indices = self.indexes[
+        batch_indices = self.indices[
                         index*batch_size:(index+1)*batch_size]
         
         X = np.empty((batch_size, *self.dim), self.X_dtype)
@@ -131,7 +151,7 @@ class MABe_Data_Generator(keras.utils.Sequence):
 
     def on_epoch_end(self):
         if self.shuffle == True:
-            np.random.shuffle(self.indexes)
+            np.random.shuffle(self.indices)
 
 
 if __name__ == "__main__":
@@ -143,7 +163,7 @@ if __name__ == "__main__":
                     pose_dict,
                     batch_size=2,
                     feature_dimensions=(2,2,7),
-                    n_classes=4,
+                    classes=['other', 'investigation', 'attack', 'mount'],
                     past_frames=100,
                     future_frames=100,
                     frame_skip=1,
