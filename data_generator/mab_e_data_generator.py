fix for mean average precision

data_generator/mab_e_data_generator.py
@@ -132,7 +132,9 @@ class MABe_Data_Generator(keras.utils.Sequence):
         self.indices = np.arange(len(self.frame_indices))
 
     def __len__(self):
-        return len(self.indices) // self.batch_size
+        ct = len(self.indices) // self.batch_size
+        ct += int((len(self.indices) % self.batch_size) > 0)
+        return ct
 
     def get_X(self, data_index):
         """
@@ -161,7 +163,7 @@ class MABe_Data_Generator(keras.utils.Sequence):
         Augment sequences
             * Rotation - All frames in the sequence are rotated by the same angle
                 using the euler rotation matrix
-            * Shift - All frames in the sequence are shifted randomly 
+            * Shift - All frames in the sequence are shifted randomly
                 but by the same amount
         """
         # Rotate
@@ -178,7 +180,9 @@ class MABe_Data_Generator(keras.utils.Sequence):
     def __getitem__(self, index):
         batch_size = self.batch_size
         batch_indices = self.indices[
-            index*batch_size:(index+1)*batch_size]
+            index*batch_size:(index+1)*batch_size]  # List indexing overflow gets clipped
+
+        batch_size = len(batch_indices)  # For the case when list indexing is clipped
 
         X = np.empty((batch_size, *self.dim), self.X_dtype)
 

train_task1.py
@@ -1,5 +1,8 @@
+import os
+import argparse
 from copy import deepcopy
 
+import tensorflow as tf
 from utils.load_data import load_mabe_data_task1
 from utils.dirs import create_dirs
 from utils.preprocessing import normalize_data, transpose_last_axis
@@ -11,7 +14,8 @@ from data_generator.mab_e_data_generator import calculate_input_dim
 from utils.save_results import save_results
 
 
-def train_task1(train_data_path, results_dir, config, test_data_path):
+def train_task1(train_data_path, results_dir, config, test_data_path,
+                pretrained_model_path=None, skip_training=False):
 
     # Load the data
     dataset, vocabulary = load_mabe_data_task1(train_data_path)
@@ -80,16 +84,24 @@ def train_task1(train_data_path, results_dir, config, test_data_path):
                       architecture=config.architecture,
                       arch_params=config.architecture_parameters)
 
-    # Model initialization
-    trainer.initialize_model(layer_channels=config.layer_channels,
-                             dropout_rate=config.dropout_rate,
-                             learning_rate=config.learning_rate)
+    # In case of only using
+    if skip_training and pretrained_model_path is not None:
+        trainer.model = tf.keras.models.load_model(pretrained_model_path)
 
-    # Print the model
-    trainer.model.summary()
+        # Print model summary
+        trainer.model.summary()
 
-    # Train model
-    trainer.train(epochs=config.epochs)
+        print("Skipping Training")
+    else:
+        # Model initialization
+        trainer.initialize_model(layer_channels=config.layer_channels,
+                                 dropout_rate=config.dropout_rate,
+                                 learning_rate=config.learning_rate)
+        # Print model summary
+        trainer.model.summary()
+
+        # Train model
+        trainer.train(epochs=config.epochs)
 
     # Get metrics
     train_metrics = trainer.get_metrics(mode='train')
@@ -104,8 +116,31 @@ def train_task1(train_data_path, results_dir, config, test_data_path):
 
 if __name__ == '__main__':
     train_data_path = 'data/task1_train_data.npy'
-    test_data_path = 'data/task1_test_data_converted.npy'
+    test_data_path = 'data/task1_test_ground_truth.npy'
     results_dir = 'results/task1_baseline'
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--seed', type=int, default=42, help='Seed value')
+    parser.add_argument('--skip-training', action="store_true",
+                        help='Only generate metrics')
+
+    parsed_args = parser.parse_args()
+    seed = parsed_args.seed
+    skip_training = parsed_args.skip_training
+
     from configs.task1_baseline import task1_baseline_config
     config = task1_baseline_config
-    train_task1(train_data_path, results_dir, config, test_data_path)
+
+    pretrained_model_path = None
+    if skip_training:
+        model_path = f'{results_dir}/task1_seed_{seed}_model.h5'
+        if not os.path.exists(model_path):
+            raise FileNotFoundError(f"Pretrained model not found - \
+                    {model_path} - Required for skip-training")
+        else:
+            pretrained_model_path = model_path
+
+    train_task1(train_data_path, results_dir,
+                config, test_data_path,
+                pretrained_model_path,
+                skip_training)

train_task2.py
@@ -1,6 +1,7 @@
 import os
+import argparse
 from copy import deepcopy
-from tensorflow import keras
+import tensorflowm as tf
 
 from utils.load_data import load_mabe_data_task2
 from utils.dirs import create_dirs
@@ -89,7 +90,7 @@ def train_task2(train_data_path, results_dir, config, pretrained_model_path):
 
         if pretrained_model_path and os.path.exists(pretrained_model_path):
             # Make trainer model as the pretrained model
-            trainer.model = keras.models.load_model(pretrained_model_path)
+            trainer.model = tf.keras.models.load_model(pretrained_model_path)
 
             # Get zero shot trainer metrics
             val_metrics_zs = trainer.get_metrics(mode='validation')

train_task3.py
@@ -1,6 +1,7 @@
 import os
+import argparse
 from copy import deepcopy
-from tensorflow import keras
+import tensorflow as tf
 
 from utils.load_data import load_mabe_data_task3
 from utils.dirs import create_dirs
@@ -94,7 +95,7 @@ def train_task3(train_data_path, results_dir, config, pretrained_model_path):
 
         if pretrained_model_path and os.path.exists(pretrained_model_path):
             # Make trainer model as the pretrained model
-            pretrained_model = keras.models.load_model(pretrained_model_path)
+            pretrained_model = tf.keras.models.load_model(pretrained_model_path)
 
             # Copy weights upto last layer
             copy_model_weights_except_last_layer(trainer.model, pretrained_model)
@@ -123,7 +124,7 @@ def train_task3(train_data_path, results_dir, config, pretrained_model_path):
         val_metrics = trainer.get_metrics(mode='validation')
 
         # Save the results
-        save_results(results_dir, f'task2_{behavior}',
+        save_results(results_dir, f'task3_{behavior}',
                      trainer.model, config,
                      train_metrics, val_metrics)
 

trainers/mab_e_trainer.py
@@ -1,8 +1,7 @@
 import numpy as np
-from tensorflow import keras
+import tensorflow as tf
 from tensorflow.keras.models import Model
 import tensorflow.keras.layers as layers
-from tensorflow.python.ops.numpy_ops.np_math_ops import average
 import tensorflow_addons as tfa
 import sklearn
 import pandas as pd
@@ -59,7 +58,7 @@ class Trainer:
         x = layers.Dense(self.num_classes, activation='softmax')(x)
 
         metrics = [tfa.metrics.F1Score(num_classes=self.num_classes)]
-        optimizer = keras.optimizers.Adam(lr=learning_rate)
+        optimizer = tf.keras.optimizers.Adam(lr=learning_rate)
 
         model = Model(inputs, x)
 
@@ -104,11 +103,15 @@ class Trainer:
         y_val = np.argmax(np.array(y_val), axis=-1)
         return y_val
 
+    def get_prediction_probabilities(self, generator):
+        """ Get all the model predictions """
+        return self.model.predict(generator, verbose=True)
+
     def get_predictions(self, generator):
         """ Get all the model predictions """
-        y_val_pred = self.model.predict(generator)
-        y_val_pred = np.argmax(y_val_pred, axis=-1)
-        return y_val_pred
+        y_pred = self.get_prediction_probabilities(generator)
+        y_pred = np.argmax(y_pred, axis=-1)
+        return y_pred
 
     def get_metrics(self, mode='validation'):
         """
@@ -117,7 +120,8 @@ class Trainer:
         """
         generator = self.get_generator_by_mode(mode)
         labels = self.get_labels(generator)
-        predictions = self.get_predictions(generator)
+        probabilites = self.get_prediction_probabilities(generator)
+        predictions = np.argmax(probabilites, axis=-1)
 
         f1_scores = sklearn.metrics.f1_score(labels, predictions, average=None)
         rec_scores = sklearn.metrics.precision_score(
@@ -129,13 +133,22 @@ class Trainer:
         ap_scores = []
         for single_label in sorted(np.unique(labels)):
             labels_l = labels == single_label
-            preds_l = predictions == single_label
+            probabilites_l = probabilites[:, single_label] 
             ap_score_l = sklearn.metrics.average_precision_score(
-                labels_l, preds_l, average='macro')
+                labels_l, probabilites_l, average='micro')
             ap_scores.append(ap_score_l)
-        
+
         classes = list(self.class_to_number.keys())
         metrics = pd.DataFrame({"Class": classes, "F1": f1_scores,
                                 "Precision": prec_scores, "Recall": rec_scores,
                                 "Average Precision": ap_scores})
+
+        if len(classes) > 2:
+            try:
+                average_scores = metrics[metrics['Class'] != 'other'].mean()
+                metrics = metrics.append(average_scores, ignore_index=True)
+                metrics.iloc[-1, 0] = 'Macro Average'
+            except Exception:
+                pass
+
         return metrics