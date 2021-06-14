93a9bc6eb47b2ede456c616647f1a6301aab49e6
jenjsun <jjsun@caltech.edu>
2021-06-14 05:19:20 +0000

    Update baseline runs

--- train_all_experiments.py
+++ train_all_experiments.py
@@ -29,65 +29,6 @@ parser.add_argument('--seed', type=int, default=42, help='Seed value')
 
 seed = parser.parse_args().seed
 
-# Task 1 Baseline - 1D CNN
-results_dir = 'results/task1_baseline'
-config = task1_baseline_config
-config.seed = seed
-train_task1(task1_train_data_path, results_dir, config, task1_test_data_path)
-
-# # Task 1 Augmented - Will be used as pretrain model for task 2
-results_dir = 'results/task1_augmented'
-config = task1_augmented_config
-config.seed = seed
-train_task1(task1_train_data_path, results_dir, config, task1_test_data_path)
-
-# Task 2 Baseline
-results_dir = 'results/task2_baseline'
-pretrained_model_path = f'results/task1_augmented/task1_seed_{seed}_model.h5'
-config = task2_baseline_config
-config.seed = seed
-train_task2(task2_train_data_path, results_dir, config,
-            pretrained_model_path, task2_test_data_path)
-
-# Task 3 Baseline
-results_dir = 'results/task3_baseline'
-pretrained_model_path = f'results/task1_augmented/task1_seed_{seed}_model.h5'
-config = task3_baseline_config
-config.seed = seed
-train_task3(task3_train_data_path, results_dir, config,
-            pretrained_model_path, task3_test_data_path)
-
-# Task 1 Fully connected
-results_dir = 'results/task1_fc'
-config = task1_fc_config
-config.seed = seed
-train_task1(task1_train_data_path, results_dir, config, task1_test_data_path)
-
-# Task 1 Attention
-results_dir = 'results/task1_attention'
-config = task1_attention_config
-config.seed = seed
-train_task1(task1_train_data_path, results_dir, config, task1_test_data_path)
-
-# Task 1 LSTM
-results_dir = 'results/task1_lstm'
-config = task1_lstm_config
-config.seed = seed
-train_task1(task1_train_data_path, results_dir, config, task1_test_data_path)
-
-# Task 1 Single Frame model
-results_dir = 'results/task1_singleframe'
-config = task1_singleframe_config
-config.seed = seed
-train_task1(task1_train_data_path, results_dir, config, task1_test_data_path)
-
-# Task 1 Causal Model
-results_dir = 'results/task1_causal'
-config = task1_causal_config
-config.seed = seed
-train_task1(task1_train_data_path, results_dir, config, task1_test_data_path)
-
-
 # Task 1 Baseline with 100% train data - 1D CNN
 results_dir = 'results/task1_train_full_data'
 config = task1_baseline_config
