68ddc283c8ebbed7cb725f1f972e639d5eba6f1d
Mengyang Liu <mliu444@gatech.edu>
2022-03-13 23:05:21 -0400

    add sinkhorn from self-label

---
+++ sinhorn/sinkhorn.py
@@ -0,0 +1,34 @@
+import numpy as np
+import time
+import torch
+import argparse as args
+
+def optimize_L_sk(PS):
+    """
+    :param PS: The prediction matrix, N x K
+    """
+    N, K = PS.shape
+    tt = time.time()
+    PS = PS.T  # now it is K x N
+    r = np.ones((K, 1)) / K
+    c = np.ones((N, 1)) / N
+    PS **= args.lamb  # K x N
+    inv_K = 1. / K
+    inv_N = 1. / N
+    err = 1e3
+    _counter = 0
+    while err > 1e-2:
+        r = inv_K / (PS @ c)  # (KxN)@(N,1) = K x 1
+        c_new = inv_N / (r.T @ PS).T  # ((1,K)@(KxN)).t() = N x 1
+        if _counter % 10 == 0:
+            err = np.nansum(np.abs(c / c_new - 1))
+        c = c_new
+        _counter += 1
+    PS *= np.squeeze(c)
+    PS = PS.T
+    PS *= np.squeeze(r)
+    PS = PS.T
+    argmaxes = np.nanargmax(PS, 0)  # size N
+    newL = torch.LongTensor(argmaxes)
+    selflabels = newL.cuda()
+    return cost, selflabels
