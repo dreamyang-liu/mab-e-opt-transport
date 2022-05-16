ac9784ae245a7007b07742aafa9065a20ea7044f
Mengyang Liu <mliu444@gatech.edu>
2022-05-16 00:36:06 -0400

    update start.sh

--- start.sh
+++ start.sh
@@ -3,16 +3,16 @@ seed=42
 device=3
 sinkhorn_device=4
 save_dir='./save'
-warmup_epoch=0
+warmup_epoch=20
 data_dir='./data/train.npy'
 lop=2
-epoch=100
+epoch=50
 batch_size=128
-num_clusters=50
+num_clusters=100
 lr=1e-3
 dist=gauss
 gauss_sd=0.1
-
+reducer=umap
 
 
 
@@ -33,3 +33,4 @@ python main.py \
 --num_clusters $num_clusters \
 --dist $dist \
 --gauss_sd $gauss_sd \
+--reducer $reducer \
